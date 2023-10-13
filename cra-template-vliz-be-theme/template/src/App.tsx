08726324bc374c431197f0585b8e12bc06bb291d
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2023-10-13 09:21:18 +0200

    added new components
    added :
    *waved carrousel item
    * text_image item

--- cra-template-vliz-be-theme/template/src/App.tsx
+++ cra-template-vliz-be-theme/template/src/App.tsx
@@ -4,18 +4,19 @@ import ItemListBlockColored from './components/item/list/block/colored/main';
 import ItemListCardColored from './components/item/list/card/colored/main';
 import ItemListCarrouselBlock from './components/item/list/carrousel/block/main';
 import ItemListGallery from './components/item/list/gallery/main';
+import ItemListCarrouselWaved from './components/item/list/carrousel/waved/main';
 import ItemListRowTextImage from './components/item/list/row/text_image/main';
 
 function App() {
 
   let itemlist = {"title":"subtile title",
   "items":[
-            {"title": "item 1","clickthrough_url":"/test"},
-            {"title": "item 2"},
-            {"title": "item 3","clickthrough_url":"/tes2"},
-            {"title": "item 4"},
-            {"title": "item 5","clickthrough_url":"/tes3"},
-            {"title": "item 6"}
+            {"title": "item 1","clickthrough_url":"/test", "description": "item 1 description", "image": "/assets/media/img/placeholder.png", "col_width_text": 9, "clickthrough_text": "test"},
+            {"title": "item 2", "description": "item 2 description", "image": "/assets/media/img/placeholder.png", "col_width_text": 6, "clickthrough_text": "test"},
+            {"title": "item 3","clickthrough_url":"/tes2", "image": "/assets/media/img/placeholder.png", "col_width_text": 10, "clickthrough_text": "test"},
+            {"title": "item 4", "description": "item 4 description", "image": "/assets/media/img/placeholder.png", "col_width_text": 11, "clickthrough_text": "test"},
+            {"title": "item 5","clickthrough_url":"/tes3", "image": "/assets/media/img/placeholder.png", "col_width_text": 7, "clickthrough_text": "test"},
+            {"title": "item 6", "description": "item 6 description", "image": "/assets/media/img/placeholder.png", "col_width_text": 8, "clickthrough_text": "test"}
           ]
   }
 
@@ -53,7 +54,9 @@ function App() {
         <ItemListBlockColored items={itemlist.items} />
         <ItemListCardColored item={itemlist} />
         <ItemListCarrouselBlock item={itemlist} />
+        <ItemListCarrouselWaved item={itemlist} />
         <ItemListGallery item={gallery} />
+        <ItemListRowTextImage item={itemlist} />
       </DefaultLayout>
     </>
   );
--- cra-template-vliz-be-theme/template/src/components/item/list/row/text_image/item.tsx
+++ cra-template-vliz-be-theme/template/src/components/item/list/row/text_image/item.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 
 export default function ItemListRowTextImageItem(props) {
     let item = props.item;
+    let rest_col_width = 12 - item.col_width_text;
+    let image_size = rest_col_width * 70;
 
     return (
             item.image ?
@@ -23,7 +25,7 @@ export default function ItemListRowTextImageItem(props) {
                     </div>
                 </div>
             </div>
-            <div class="col-lg-{{ 12 | minus:col_width_text }} field__item">
+            <div class={`col-lg-${rest_col_width} field__item`}>
                 <div class="paragraph paragraph--type--media paragraph--view-mode--default">
                     <div class="field field--name-field-media field--type-entity-reference field--label-hidden field__items">
                         <article class="media media--type-image media--view-mode-large">
@@ -33,7 +35,13 @@ export default function ItemListRowTextImageItem(props) {
                                     alt={ item.title } 
                                     typeof="foaf:Image" 
                                     class="image-style-large-1000" 
-                                    style="width: {{ 12 | minus:col_width_text | times:70}}px; display:block;margin:auto;vertical-align:middle;max-width:300px;"
+                                    style={{
+                                        width: `${image_size}px`,
+                                        display: 'block',
+                                        margin: 'auto',
+                                        verticalAlign: 'middle',
+                                        maxWidth: '300px'
+                                    }}
                                 />
                             </div>
                         </article>
